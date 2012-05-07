138e5090d30dbea7c3d57d24171a8f0852380014
Joao Martins <joaovitor.martins@gmail.com>
2012-05-07 20:09:49 -0300

    commit chrome extesion base

---
+++ src/email_sender.py
@@ -0,0 +1,34 @@
+fromAddress = 'toreadl@gmail.com'
+toAddress = 'joaovitor.martins@gmail.com'
+
+import socket
+import smtplib
+from smtplib import SMTP
+from email.MIMEText import MIMEText
+
+try:
+	smtp = SMTP()
+	print("Before connection...")
+	smtp.connect('smtp.gmail.com')
+	smtp.starttls() 
+	smtp.ehlo()
+	print("After connection...")
+	smtp.login('toreadl@gmail.com', 'codified')
+	
+	# Open a plain text file for reading.  For this example, assume that
+	# the text file contains only ASCII characters.
+	# fp = open('textfile.txt', 'rb')
+	# Create a text/plain message
+	# msg = MIMEText(fp.read())
+	# fp.close()
+	
+	msg = MIMEText("Este eh o conteudo. Motherfucker! Base para o plugin criada!")
+	msg['Subject'] = '[ToRead] Daily Reading'
+	msg['From'] = 'ToRead Later'
+	msg['To'] = toAddress;
+	msg['Content-Type'] = 'text/plain';
+	smtp.sendmail(fromAddress, toAddress, msg.as_string())
+	smtp.quit()
+	
+except (smtplib.SMTPException, socket.error), arg:
+    print "SMTP Server could not send mail", arg
